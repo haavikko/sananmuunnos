332d937bb28d586b9c7536b22eb897940b0828df
Matti Haavikko <haavikko@gmail.com>
2020-10-02 13:21:04 +0300

    project naming

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sananmuunnos project.
+ASGI config for word_transform project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sananmuunnos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()

b4c7f2b11faa5fe4f1577f1efe0823ed647242a3
Matti Haavikko <haavikko@gmail.com>
2020-10-02 13:15:38 +0300

    word_transform view function

--- word_transform/views.py
+++ word_transform/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
 
-from .models import Greeting
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+from .transform import transform_words, WordTransformException, WordTransformLogicError
 
 
-def db(request):
+@csrf_exempt
+@require_http_methods(["POST"])
+def word_transform(request):
+    '''
+    Main end point of the application.
 
-    greeting = Greeting()
-    greeting.save()
+    :param request: must contain utf-8 encoded json string in the request.body
+    :return: HttpResponse
+    '''
+    try:
+        request_body_unicode = request.body.decode('utf-8', 'strict')
+    except UnicodeDecodeError:
+        return HttpResponseBadRequest('utf-8 formatted input string required')
 
-    greetings = Greeting.objects.all()
+    try:
+        response_data = transform_words(request_body_unicode)
+    except WordTransformLogicError as e:
+        return HttpResponseServerError('Could not process the request')
+    except WordTransformException as e:
+        return HttpResponseBadRequest('Invalid request')
+    except Exception as e:
+        return HttpResponseServerError('Server error')
 
-    return render(request, "db.html", {"greetings": greetings})
+    return HttpResponse(response_data, content_type='application/json')
